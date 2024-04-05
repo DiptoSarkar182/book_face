086d20ffc86f9631428273dc1defe8bc28beaf1b
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-04-06 00:10:39 +0600

    added post upload with image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,23 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order('created_at DESC')
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = current_user.posts.build(post_params)
+    respond_to do |format|
+      if @post.save
+        format.html {redirect_to root_path, notice: "Post created successfully"}
+      else
+        format.html {render 'new', status: :unprocessable_entity}
+      end
+    end
+  end
 
+  private
+  def post_params
+    params.require(:post).permit(:body, :post_image)
   end
 end
