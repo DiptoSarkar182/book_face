ed04730f273f6fda7a97c5f4edb2e3d1ca99f22d
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-04-06 14:17:35 +0600

    added a feature when user deletes post, it also deletes imagefrom cloudinary

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :post_image
+
+  before_destroy :purge_post_image
+
+  private
+
+  def purge_post_image
+    post_image.purge
+  end
 end
