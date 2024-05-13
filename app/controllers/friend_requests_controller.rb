6df892d8fe9d7d0c86fb55740c54b9cedc02d4bb
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-05-14 01:15:04 +0600

    removed friend_requests_path from layout, added users searching in the layout, turbo frame for add/cancel friend button, moved people you may know to friends list

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,26 +1,29 @@
 class FriendRequestsController < ApplicationController
+
+  # /friend_requests
   def index
-    @users = User.where.not(id: current_user.id)
-                 .where.not(id: current_user.friends.select(:id))
+    redirect_to root_path, alert: "Search users by the search box."
   end
+
+  # /friend_requests
   def create
     @friend_request = FriendRequest.new(sender: current_user, receiver_id: params[:receiver_id])
-    respond_to do |format|
-      if @friend_request.save
-        format.html {redirect_to friend_requests_path, notice: "Friend request sent!"}
-      else
-        format.html {redirect_to friend_requests_path, alert: "Friend request could not be sent."}
-      end
+    if @friend_request.save
+      render partial: "add_cancel_friend", locals: { user: @friend_request.receiver }
+    else
+      redirect_to friend_requests_path, alert: "Friend request could not be sent."
     end
   end
 
+  # /friend_requests/:id
   def destroy
     @friend_request = FriendRequest.find(params[:id])
+    user = @friend_request.receiver
     @friend_request.destroy
-    respond_to do |format|
-      format.html { redirect_to friend_requests_path, notice: "Friend request cancelled." }
-    end
+    render partial: "add_cancel_friend", locals: { user: user }
   end
+
+  # /friend_requests/:id/accept
   def accept
     friend_request = current_user.received_friend_requests.find_by(sender_id: params[:id])
     if friend_request
@@ -32,6 +35,8 @@ class FriendRequestsController < ApplicationController
       redirect_to notifications_path, alert: "Friend request not found."
     end
   end
+
+  # /friend_requests/:id/reject
   def reject
     friend_request = current_user.received_friend_requests.find_by(sender_id: params[:id])
     if friend_request
@@ -42,7 +47,7 @@ class FriendRequestsController < ApplicationController
     end
   end
 
-
+  # /friend_requests/search_friend
   def search_friend
     search_term = params[:name]
     @name_search_results = User.where("first_name ILIKE :search OR last_name ILIKE :search OR CONCAT(first_name, ' ', last_name) ILIKE :search", search: "%#{search_term}%")
--- app/controllers/friends_list_controller.rb
+++ app/controllers/friends_list_controller.rb
@@ -1,6 +1,10 @@
 class FriendsListController < ApplicationController
   def index
     @friends = current_user.friends
+    @users = User.where.not(id: current_user.id)
+                 .where.not(id: current_user.friends.select(:id))
+                 .order("RANDOM()")
+                 .limit(10)
   end
   def destroy
     friend = User.find(params[:id])
